5593c167186a6ca09706544e2a174949fc6649ee
Masud <masudbook@gmail.com>
2018-10-20 13:52:38 +0200

    initial

---
+++ src/main/java/online/smartcompute/eclipseplugindemo/eclipseplugins/App.java
@@ -0,0 +1,13 @@
+package online.smartcompute.eclipseplugindemo.eclipseplugins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
